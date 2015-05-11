e82db713cb9fd16a73c234cc08487f4560df0df4
squirrelxseeker <patrickbogucki@gmail.com>
2015-05-11 06:58:55 -0700

    Adding and deleting activities now works.

--- scripts.js
+++ scripts.js
@@ -3,16 +3,28 @@ var main = function() {
 	$('.add').click(function() {
 		console.log($('.form-control').val());
 		activity = $('.form-control').val();
-		$('ul').append('<li class="activity-item">' + activity + '</li>			<button class="btn btn-default delete">Delete</button>');
+		$('ul').append('<li class="activity-item">' + activity + '<button class="btn btn-default delete">Delete</button> </li>');
 		$('.delete').click(function() {
-			$(this).remove();
+			$(this).parent().fadeOut('slow', function() {
+				$(this).remove();
+			});
+			if(!$('.activities').has('li')) {
+				$('.empty-text').removeClass('empty')
+			}
 		});
-		$('.empty-text').hide();
+		$('.empty-text').addClass('empty');
+		
 	});
 
 	$('.delete').on('click', function() {
 		console.log('hello world');
 		$('.activity-item').remove();
+	});
+	$('.delete-all').on('click', function() {
+		$('.activity-item').fadeOut('slow', function() {
+			$(this).remove();
+			$('.empty-text').removeClass('empty');
+		});
 	});
 // var newActivityItem = 
 
